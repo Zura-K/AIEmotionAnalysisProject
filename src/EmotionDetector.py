94aa535bcdc84868d47ec9e974832988618e8a08
zurab kapanadze <zurabkapanadze97@hotmail.com>
2025-02-16 02:57:34 +0400

    Push, Because Apparently, this Course HATES separate IDEs

--- src/EmotionDetector.py
+++ src/EmotionDetector.py
@@ -10,6 +10,22 @@ class EmotionDetector:
         response = requests.post(self.url, headers=self.headers, json=payload)
 
         if response.status_code == 200:
-            return response.json()
+            return self.format_response(response.json())
         else:
             return {"error": "Failed to fetch emotion analysis"}
+
+    @staticmethod
+    def format_response(res):
+        if not res:
+            return {"error": "No emotions detected"}
+
+        dominant_emotion = max(res, key=res.get)
+
+        return {
+            'anger': res.get('anger', 0.0),
+            'disgust': res.get('disgust', 0.0),
+            'fear': res.get('fear', 0.0),
+            'joy': res.get('joy', 0.0),
+            'sadness': res.get('sadness', 0.0),
+            'dominant_emotion': dominant_emotion
+        }
